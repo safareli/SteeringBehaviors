40a6683c96be320e1ca17b2cec63f7b511678ee2
Irakli Safareli <sapa8@facebook.com>
2013-09-30 02:49:57 +0400

    add PVector class from cProcessing library

---
+++ src/PVector.cpp
@@ -0,0 +1,3 @@
+#include "../include/PVector.h"
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "../include/Number.h"
 #include "../include/Random.h"
 #include "../include/Color.h"
+#include "../include/PVector.h"
 #include <ctime>
 
 using namespace std;
@@ -11,11 +12,16 @@ int main(){
     Number mRandG(10,255);
     Number mRandB(0,10);
     Number mRandA(255);
-
+    PVector vec1(10,5);
+    PVector vec2(-5,5);
     Color mColor(mRandR,mRandG,mRandB,mRandA);
     mColor.setNoise(0.01);
     mColor.regenerate();
+    PVector v = vec1 + vec2;
     std::cout << mColor.get() << "\n";
+    std::cout << v.x << "\n";
+    std::cout << v.y << "\n";
+    std::cout << v.z << "\n";
 
     /*float max=0,min=0;
     for(float i = 0; i <10 ; i +=0.01){
